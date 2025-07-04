b83284130ffc0727bb0ff0d0bb8c38b5f405e0d5
Jeimyramos <ramoscoaquirajeimypaul@gmail.com>
2025-07-04 01:51:08 -0600

    vamos

--- ms_compras/src/main/java/com/example/ms_producto/service/ComprasService.java
+++ ms_compras/src/main/java/com/example/ms_producto/service/ComprasService.java
@@ -4,7 +4,7 @@ import com.example.ms_compras.entity.Compras;
 
 import java.util.List;
 
-public interface ComprasService {Compras
+public interface ComprasService {
     List<Compras> findAll();
     Compras findById(int id);
     Compras Save(Compras compras);
